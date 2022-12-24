e0e0e4d8b4335d9b0265db4505dc1a83c0081b2f
sherif <sherifahmedelsayed2@gmail.com>
2022-12-25 00:27:26 +0200

    add the mobile app

---
+++ Mobile App/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mi_project/screens/discovery_page.dart';
+import 'package:mi_project/screens/main_page.dart';
+import 'package:mi_project/screens/select_bonded_device_page.dart';
+import 'package:mi_project/screens/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData.dark(),
+      routes: {
+        HomePage.id: (context) => const HomePage(),
+        MainPage.id: (context) => const MainPage(),
+        DiscoveryPage.id: (context) => const DiscoveryPage(),
+        SelectBondedDevicePage.id: (context) => const SelectBondedDevicePage()
+      },
+    );
+  }
+}
---
+++ Mobile App/lib/screens/ChatPage.dart
@@ -0,0 +1,257 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+class ChatPage extends StatefulWidget {
+  static String id = "ChatPageID";
+
+  final BluetoothDevice server;
+
+  const ChatPage({super.key, required this.server});
+
+  @override
+  _ChatPage createState() => _ChatPage();
+}
+
+class _Message {
+  int whom;
+  String text;
+
+  _Message(this.whom, this.text);
+}
+
+class _ChatPage extends State<ChatPage> {
+  static final clientID = 0;
+  BluetoothConnection? connection;
+
+  bool isIncubatorOn = false;
+  List parametersReceived = [];
+
+  // List<_Message> messages = List<_Message>.empty(growable: true);
+  String receivedMessageText = "";
+  String _messageBuffer = '';
+  bool isWarning = false;
+
+  final TextEditingController textEditingController = TextEditingController();
+
+  bool isConnecting = true;
+  bool get isConnected => (connection?.isConnected ?? false);
+
+  bool isDisconnecting = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    BluetoothConnection.toAddress(widget.server.address).then((_connection) {
+      print('Connected to the device');
+      connection = _connection;
+      setState(() {
+        isConnecting = false;
+        isDisconnecting = false;
+      });
+
+      connection!.input!.listen(_onDataReceived).onDone(() {
+        // Example: Detect which side closed the connection
+        // There should be `isDisconnecting` flag to show are we are (locally)
+        // in middle of disconnecting process, should be set before calling
+        // `dispose`, `finish` or `close`, which all causes to disconnect.
+        // If we except the disconnection, `onDone` should be fired as result.
+        // If we didn't except this (no flag set), it means closing by remote.
+        if (isDisconnecting) {
+          print('Disconnecting locally!');
+        } else {
+          print('Disconnected remotely!');
+        }
+        if (this.mounted) {
+          setState(() {});
+        }
+      });
+    }).catchError((error) {
+      print('Cannot connect, exception occured');
+      print(error);
+    });
+  }
+
+  @override
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and disconnect
+    if (isConnected) {
+      isDisconnecting = true;
+      connection?.dispose();
+      connection = null;
+    }
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final serverName = widget.server.name ?? "Unknown";
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.pink,
+          title: (isConnecting
+              ? Text('Connecting chat to $serverName...')
+              : isConnected
+                  ? Text('Live chat with $serverName')
+                  : Text('Chat log with $serverName'))),
+      body: Visibility(
+        visible: isIncubatorOn,
+        replacement: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Turn the incubator on",
+                style: TextStyle(fontSize: 20),
+              ),
+              IconButton(
+                onPressed: () => setState(() {
+                  !isIncubatorOn
+                      ? {isIncubatorOn = true, _sendMessage("on")}
+                      : null;
+                }),
+                icon: const Icon(Icons.power_settings_new_rounded),
+                iconSize: 150,
+              ),
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.pink, width: 5)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          parametersReceived.isNotEmpty
+                              ? "Temperature: ${parametersReceived[0]} °C"
+                              : "No Temperature",
+                          style: const TextStyle(fontSize: 25),
+                        ),
+                        const SizedBox(height: 30),
+                        Text(
+                          parametersReceived.isNotEmpty
+                              ? "Humidity: ${parametersReceived[1]} %"
+                              : "No Humidity",
+                          style: const TextStyle(fontSize: 25),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue[900]),
+                        onPressed: () => isConnected ? _sendMessage("1") : null,
+                        child: Text("Send 1")),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue[900]),
+                        onPressed: () => isConnected ? _sendMessage("2") : null,
+                        child: Text("Send 2")),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue[900]),
+                        onPressed: () => isConnected ? _sendMessage("3") : null,
+                        child: Text("Send 3")),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue[900]),
+                        onPressed: () => isConnected ? _sendMessage("4") : null,
+                        child: Text("Send 4")),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onDataReceived(Uint8List data) {
+    // Allocate buffer for parsed data
+    int backspacesCounter = 0;
+    for (var byte in data) {
+      if (byte == 8 || byte == 127) {
+        backspacesCounter++;
+      }
+    }
+    Uint8List buffer = Uint8List(data.length - backspacesCounter);
+    int bufferIndex = buffer.length;
+
+    // Apply backspace control character
+    backspacesCounter = 0;
+    for (int i = data.length - 1; i >= 0; i--) {
+      if (data[i] == 8 || data[i] == 127) {
+        backspacesCounter++;
+      } else {
+        if (backspacesCounter > 0) {
+          backspacesCounter--;
+        } else {
+          buffer[--bufferIndex] = data[i];
+        }
+      }
+    }
+
+    // Create message if there is new line character
+    String dataString = String.fromCharCodes(buffer);
+    int index = buffer.indexOf(13);
+    if (~index != 0) {
+      setState(() {
+        receivedMessageText = backspacesCounter > 0
+            ? _messageBuffer.substring(
+                0, _messageBuffer.length - backspacesCounter)
+            : _messageBuffer + dataString.substring(0, index);
+
+        _messageBuffer = dataString.substring(index);
+        // Check incoming message
+        if (receivedMessageText.isNotEmpty) {
+          parametersReceived = receivedMessageText.split(" ");
+          isIncubatorOn = true;
+        }
+      });
+    } else {
+      _messageBuffer = (backspacesCounter > 0
+          ? _messageBuffer.substring(
+              0, _messageBuffer.length - backspacesCounter)
+          : _messageBuffer + dataString);
+    }
+  }
+
+  void _sendMessage(String text) async {
+    text = text.trim();
+    textEditingController.clear();
+
+    if (text.isNotEmpty) {
+      try {
+        connection!.output.add(Uint8List.fromList(utf8.encode("$text\r\n")));
+        await connection!.output.allSent;
+
+        // show message sent to arduino
+        // setState(() {
+        //   messages.add(_Message(clientID, text));
+        // });
+      } catch (e) {
+        // Ignore error, but notify state
+        setState(() {});
+      }
+    }
+  }
+}
